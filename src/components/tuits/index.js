aeda37e937541e54d69a47c848e728c641014ea6
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-11-20 13:58:29 -0500

    Dislike button

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -2,16 +2,22 @@ import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
+import * as dislikesService from "../../services/dislike-service";
 import { findAllTuits } from "../../services/tuits-service";
 
 const Tuits = ({tuits = [], deleteTuit, refreshTuits}) => {
-
-  console.log("REFRESF ", refreshTuits);
   const likeTuit = (tuit) =>
     likesService
       .userTogglesTuitLikes("me", tuit._id)
       .then(refreshTuits)
-      .catch(e => alert(e))
+      .catch(e => console.log(e))
+
+  const dislikeTuit = (tuit) =>
+    dislikesService.userTogglesTuitDislikes("me", tuit._id)
+        .then(refreshTuits)
+        .catch(e => 
+          console.log(e)
+        )
 
   return (
     <div>
@@ -19,7 +25,7 @@ const Tuits = ({tuits = [], deleteTuit, refreshTuits}) => {
         {
           tuits.map && tuits.map(tuit => {
             return (
-              <Tuit key={tuit._id} deleteTuit={deleteTuit} likeTuit={likeTuit} tuit={tuit}/>
+              <Tuit key={tuit._id} deleteTuit={deleteTuit} likeTuit={likeTuit} tuit={tuit} dislikeTuit={dislikeTuit}/>
             );
           })
         }
