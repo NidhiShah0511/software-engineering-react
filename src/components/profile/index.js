634c0fe5f639e55da95abdfad3b5e986268e1e3e
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-11-20 15:51:01 -0500

    Dislike screen

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,6 +3,7 @@ import MyTuits from "./my-tuits";
 import {HashRouter, Link, Route, Routes, useNavigate, useLocation} from "react-router-dom";
 import * as service from "../../services/auth-service"
 import MyLikes from "./my-likes";
+import MyDislikes from "./my-dislikes";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -85,8 +86,13 @@ const Profile = () => {
             </li>
             <li className="nav-item">
               <Link to="/profile/mylikes"
-                    className={`nav-link ${location.pathname.indexOf('my-likes') >= 0 ? 'active':''}`}>
+                    className={`nav-link ${location.pathname.indexOf('mylikes') >= 0 ? 'active':''}`}>
                 Likes</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile/mydislikes"
+                    className={`nav-link ${location.pathname.indexOf('mydislikes') >= 0 ? 'active':''}`}>
+                Dislikes</Link>
             </li>
           </ul>
         </div>
@@ -98,6 +104,8 @@ const Profile = () => {
         <Route path="/mylikes"
                element={<MyLikes/>}/>
 
+        <Route path="/mydislikes"
+               element={<MyDislikes/>}/>
       </Routes>
 
 
