4825129df00e2ff767815f7fb7def9acb767dd5f
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-11-19 22:42:19 -0500

    Authentication files

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,12 @@
 import axios from "axios";
-const BASE_URL = "https://tuit-application-nshah.herokuapp.com";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 //const BASE_URL = "http://localhost:4000";
 const TUITS_API = `${BASE_URL}/tuits`;
 const USERS_API = `${BASE_URL}/users`;
+
+const api = axios.create({
+  withCredentials: true
+});
 
 export const findAllTuits = () =>
   axios.get(TUITS_API)
@@ -13,11 +17,11 @@ export const findTuitById = (tid) =>
     .then(response => response.data);
 
 export const findTuitByUser = (uid) =>
-  axios.get(`${USERS_API}/${uid}/tuits`)
+  api.get(`${USERS_API}/${uid}/tuits`)
     .then(response => response.data);
 
 export const createTuitByUser = (uid, tuit) =>
-  axios.post(`${USERS_API}/${uid}/tuits`, {"tuit": tuit})
+  api.post(`${USERS_API}/${uid}/tuits`, {"tuit": tuit})
     .then(response => response.data);
 
 export const createTuit = (uid, tuit) =>
